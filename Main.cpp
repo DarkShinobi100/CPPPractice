1b8ec7cde66a05a17fb93e40ca80869c5d2de29e
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-09-05 14:28:08 +0100

    Added base main.CPP

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+
+
+//entry point for our program
+//every C++ program needas a main() function
+
+int main()
+{
+	//tell the operasting system we are done
+	//and there was no error
+	return 0;
+}
